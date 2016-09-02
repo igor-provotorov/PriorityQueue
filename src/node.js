4b33f49784229566057930ddd560e884a5ff92d4
harlach-marina <marina.harlach@gmail.com>
2016-09-02 10:19:15 +0300

    added constructor, appendChild and removeChild methods to Node class

--- src/node.js
+++ src/node.js
@@ -1,18 +1,35 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
-	appendChild(node) {
-
+	appendChild(node) {		
+		if (!this.left) {
+			this.left = node;
+		} else if (!this.right) {
+			this.right = node;
+		} else if (this.left && this.right){}
 	}
 
 	removeChild(node) {
+		if (this.left != node && this.right != node) {
+			throw Exeption("Error!");
+		}
+		if (this.left == node) {
+			this.left = null;
+		}
+		if (this.right == node) {
+			this.right = null;
+		} 
 
 	}
 
 	remove() {
-
+	
 	}
 
 	swapWithParent() {
